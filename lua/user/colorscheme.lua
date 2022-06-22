d86f4881a89ff3abc11c80a0f18ab6f43a9e1082
Neveon <41444780+Neveon@users.noreply.github.com>
2022-06-22 01:25:09 -0400

    My preferences updated

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_command([[
+  augroup ChangeBackgroundColour
+    autocmd colorscheme * :hi normal guibg=#1c1c1c
+  augroup END
+]])
+vim.o.termguicolors = true
+
 vim.cmd [[
 try
   colorscheme darkplus
