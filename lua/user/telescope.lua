666e47bdeae105be25334f67471a16e2917a7847
Neveon <41444780+Neveon@users.noreply.github.com>
2022-06-22 00:37:43 -0400

    updates and preferences

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -3,8 +3,6 @@ if not status_ok then
   return
 end
 
-telescope.load_extension('media_files')
-
 local actions = require "telescope.actions"
 
 telescope.setup {
@@ -15,6 +13,7 @@ telescope.setup {
     path_display = { "smart" },
 
     mappings = {
+      -- insert mode
       i = {
         ["<C-n>"] = actions.cycle_history_next,
         ["<C-p>"] = actions.cycle_history_prev,
@@ -46,6 +45,7 @@ telescope.setup {
         ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
       },
 
+      -- normal mode
       n = {
         ["<esc>"] = actions.close,
         ["<CR>"] = actions.select_default,
@@ -79,7 +79,7 @@ telescope.setup {
       },
     },
   },
-  pickers = {
+  -- pickers = {
     -- Default configuration for builtin pickers goes here:
     -- picker_name = {
     --   picker_config_key = value,
@@ -87,18 +87,10 @@ telescope.setup {
     -- }
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
-  },
-  extensions = {
-    media_files = {
-        -- filetypes whitelist
-        -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-        filetypes = {"png", "webp", "jpg", "jpeg"},
-        find_cmd = "rg" -- find command (defaults to `fd`)
-      }
+  -- },
     -- Your extension configuration goes here:
     -- extension_name = {
     --   extension_config_key = value,
     -- }
     -- please take a look at the readme of the extension you want to configure
-  },
 }
