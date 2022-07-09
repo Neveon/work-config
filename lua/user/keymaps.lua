5857f8997c7857ecf3311473c9990deb51e73ec8
Neveon <41444780+Neveon@users.noreply.github.com>
2022-07-08 21:17:18 -0400

    Added copilot

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -10,6 +10,25 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Github Copilot
+vim.g.copilot_no_tab_map = true
+-- vim.g.copilot_assume_mapped = true
+-- vim.g.copilot_tab_fallback = ""
+vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+vim.g.copilot_filetypes = {
+  ["*"] = false,
+  ["javascript"] = true,
+  ["typescript"] = true,
+  ["lua"] = false,
+  ["rust"] = true,
+  ["c"] = true,
+  ["c#"] = true,
+  ["c++"] = true,
+  ["go"] = true,
+  ["python"] = true,
+}
+
 -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
